277959bd5ff4f2b9f016e9c4489eb260fa753980
alejandro rodriguez diaz <arodriguez@reappciona.net>
2025-04-05 13:49:01 +0200

    correcion de makefile, minishell y executors

---
+++ trabajoAlex/executors/exec_cmd.c
@@ -0,0 +1,56 @@
+#include "minishell.h"
+
+char	*get_path(char *cmd, char **envp)
+{
+	char	**paths;
+	char	*full_path;
+	char	*temp;
+	int		i = 0;
+
+	while (envp[i] && strncmp(envp[i], "PATH=", 5) != 0)
+		i++;
+	if (!envp[i])
+		return (NULL);
+	paths = ft_split(envp[i] + 5, ':');
+	i = 0;
+	while (paths[i])
+	{
+		temp = ft_strjoin(paths[i], "/");
+		full_path = ft_strjoin(temp, cmd);
+		free(temp);
+		if (access(full_path, X_OK) == 0)
+		{
+			ft_free_split(paths);
+			return (full_path);
+		}
+		free(full_path);
+		i++;
+	}
+	ft_free_split(paths);
+	return (NULL);
+}
+
+void	execute_command(char *cmd_line, char **envp)
+{
+	char	**argv = ft_split(cmd_line, ' ');
+	char	*cmd_path = get_path(argv[0], envp);
+	pid_t	pid;
+
+	if (!cmd_path)
+	{
+		printf("%s: command not found\n", argv[0]);
+		ft_free_split(argv);
+		return;
+	}
+	pid = fork();
+	if (pid == 0)
+	{
+		execve(cmd_path, argv, envp);
+		perror("execve");
+		exit(EXIT_FAILURE);
+	}
+	else
+		wait(NULL);
+	free(cmd_path);
+	ft_free_split(argv);
+}
--- trabajoAlex/minishell.h
+++ trabajoAlex/minishell.h
@@ -1,3 +1,25 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include <sys/wait.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+// Funciones de ejecución
+void	execute_command(char *cmd_line, char **envp);
+char	*get_path(char *cmd, char **envp);
+
+// Utils
+char	**ft_split(char const *s, char c);
+char	*ft_strjoin(char const *s1, char const *s2);
+void	ft_free_split(char **arr);
+
+
+// funciones de builtin
 int builtin_echo(char **args);
 int builtin_cd(char **args, char **env);
 int builtin_pwd(char **env);
@@ -5,3 +27,5 @@ int builtin_export(char **args, char ***env);
 int builtin_unset(char **args, char ***env);
 int builtin_env(char **env);
 int builtin_exit(char **args);
+
+#endif
