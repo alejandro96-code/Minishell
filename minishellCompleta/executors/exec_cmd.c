b6f5a364e1fde63483ed28abc89bcbf7ee0e9856
alejandro rodriguez diaz <arodriguez@reappciona.net>
2025-04-11 16:13:44 +0200

    variables cambiadas

--- minishellCompleta/executors/exec_cmd.c
+++ minishellCompleta/executors/exec_cmd.c
@@ -2,15 +2,15 @@
 
 void	ft_free_split(char **split)
 {
-	int	i;
+	int	cont;
 
 	if (!split)
 		return;
-	i = 0;
-	while (split[i])
+	cont = 0;
+	while (split[cont])
 	{
-		free(split[i]);
-		i++;
+		free(split[cont]);
+		cont++;
 	}
 	free(split);
 }
