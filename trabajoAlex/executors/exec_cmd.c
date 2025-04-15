c26fb520280273c02965fc7c7b4a5749a5d610d8
alejandro <alejandro1996rd@gmail.com>
2025-04-15 20:53:04 +0200

    correccion norminette en exe_cmd y minishell

--- trabajoAlex/executors/exec_cmd.c
+++ trabajoAlex/executors/exec_cmd.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exec_cmd.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alejandro <alejandro@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/29 23:56:33 by dgasco-g          #+#    #+#             */
+/*   Updated: 2025/04/15 20:51:45 by alejandro        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../minishell.h"
 
 void	ft_free_split(char **split)
@@ -5,7 +17,7 @@ void	ft_free_split(char **split)
 	int	cont;
 
 	if (!split)
-		return;
+		return ;
 	cont = 0;
 	while (split[cont])
 	{
@@ -15,48 +27,65 @@ void	ft_free_split(char **split)
 	free(split);
 }
 
-
 char	*get_path(char *cmd, char **envp)
 {
 	char	**paths;
 	char	*full_path;
-	char	*temp;
-	int		cont = 0;
 
-	while (envp[cont] && ft_strncmp(envp[cont], "PATH=", 5) != 0)
-		cont++;
-	if (!envp[cont])
+	paths = extract_paths(envp);
+	full_path = NULL;
+	if (!paths)
 		return (NULL);
-	paths = ft_split(envp[cont] + 5, ':');
-	cont = 0;
-	while (paths[cont])
+	full_path = search_in_paths(paths, cmd);
+	ft_free_split(paths);
+	return (full_path);
+}
+
+char	**extract_paths(char **envp)
+{
+	int	i;
+
+	i = 0;
+	while (envp[i] && ft_strncmp(envp[i], "PATH=", 5) != 0)
+		i++;
+	if (!envp[i])
+		return (NULL);
+	return (ft_split(envp[i] + 5, ':'));
+}
+
+char	*search_in_paths(char **paths, char *cmd)
+{
+	int		i;
+	char	*temp;
+	char	*full_path;
+
+	i = 0;
+	while (paths[i])
 	{
-		temp = ft_strjoin(paths[cont], "/");
+		temp = ft_strjoin(paths[i], "/");
 		full_path = ft_strjoin(temp, cmd);
 		free(temp);
 		if (access(full_path, X_OK) == 0)
-		{
-			ft_free_split(paths);
 			return (full_path);
-		}
 		free(full_path);
-		cont++;
+		i++;
 	}
-	ft_free_split(paths);
 	return (NULL);
 }
 
 void	execute_command(char *cmd_line, char **envp)
 {
-	char	**argv = ft_split(cmd_line, ' ');
-	char	*cmd_path = get_path(argv[0], envp);
+	char	**argv;
+	char	*cmd_path;
 	pid_t	pid;
 
+	argv = ft_split(cmd_line, ' ');
+	cmd_path = get_path(argv[0], envp);
 	if (!cmd_path)
 	{
 		printf("%s: command not found\n", argv[0]);
 		ft_free_split(argv);
-		return;
+		return ;
 	}
 	pid = fork();
 	if (pid == 0)
--- trabajoAlex/minishell.h
+++ trabajoAlex/minishell.h
@@ -1,107 +1,109 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include "libft/libft.h"
+# include <ctype.h>
+# include <dirent.h>
+# include <fcntl.h>
+# include <readline/history.h>
+# include <readline/readline.h>
 # include <stdio.h>
 # include <stdlib.h>
-# include <unistd.h>
 # include <string.h>
 # include <sys/wait.h>
-# include <ctype.h>
-# include <fcntl.h>
-# include <dirent.h>
-# include <readline/readline.h>
-# include <readline/history.h>
-# include "libft/libft.h"
+# include <unistd.h>
 # define RED "\033[31m"    // Rojo
 # define YELLOW "\033[33m" // Amarillo
 # define RESET "\033[0m"   // Reset
 
-
-typedef struct s_command {
-    char **argv;     // Array de tokens (argumentos)
-    int argc;        // Número de argumentos
-    int is_builtin;  // Indica si es un comando builtin (1 si lo es, 0 si no)
-} t_command;
+typedef struct s_command
+{
+	char **argv;    // Array de tokens (argumentos)
+	int argc;       // Número de argumentos
+	int is_builtin; // Indica si es un comando builtin (1 si lo es, 0 si no)
+}			t_command;
 
 // builtin del CD
-char *get_env_var(char *name, char **env);
-int builtin_cd(char **args, char **env);
-char *resolve_home_path(char *path, char **env);
-char *resolve_relative_path(char *path);
-int try_change_directory(char *path);
+char		*get_env_var(char *name, char **env);
+int			builtin_cd(char **args, char **env);
+char		*resolve_home_path(char *path, char **env);
+char		*resolve_relative_path(char *path);
+int			try_change_directory(char *path);
 
 // builtin del echo, env, exit y pwd
-int builtin_echo(char **args, char **env);
-int builtin_env(char **env);
-int builtin_exit(char **args);
-int builtin_pwd(char **env);
+int			builtin_echo(char **args, char **env);
+int			builtin_env(char **env);
+int			builtin_exit(char **args);
+int			builtin_pwd(char **env);
 
 // builtin del export
-int builtin_export(char **args, char ***env);
-int	handle_export(char *arg, char ***env);
-int	replace_env_value(char *key, char *new_value, char ***env);
-int	add_env_value(char *new_value, char ***env);
+int			builtin_export(char **args, char ***env);
+int			handle_export(char *arg, char ***env);
+int			replace_env_value(char *key, char *new_value, char ***env);
+int			add_env_value(char *new_value, char ***env);
 
 // builtin del unset
-int builtin_unset(char **args, char ***env);
-void unset_variable(char *var, char ***env);
-void remove_env_entry(int index, char ***env);
+int			builtin_unset(char **args, char ***env);
+void		unset_variable(char *var, char ***env);
+void		remove_env_entry(int index, char ***env);
 
-//general_build.c
-int is_builtin(char *cmd);
-int execute_builtin(char **args, char ***env);
-void execute_external(char **args, char **env);
+// general_build.c
+int			is_builtin(char *cmd);
+int			execute_builtin(char **args, char ***env);
+void		execute_external(char **args, char **env);
 
 // exec_cmd.c
-void	ft_free_split(char **split);
-char	*get_path(char *cmd, char **envp);
-void	execute_command(char *cmd_line, char **envp);
+void		ft_free_split(char **split);
+char		*get_path(char *cmd, char **envp);
+char		**extract_paths(char **envp);
+char		*search_in_paths(char **paths, char *cmd);
+void		execute_command(char *cmd_line, char **envp);
 
-//parser.c
-int is_builtin_command(const char *cmd);
-t_command *parse_input(const char *input);
+// parser.c
+int			is_builtin_command(const char *cmd);
+t_command	*parse_input(const char *input);
 
-//funciones del main
-char **copy_env(char **envp);
-char *remove_quotes(char *str);
-char *clean_input(char *input);
-char	*get_prompt(char ** env);
-void process_input(char *input, char ***env);
-int main(int argc, char **argv, char **envp);
+// funciones del main
+char		**copy_env(char **envp);
+char		*remove_quotes(char *str);
+char		*clean_input(char *input);
+char		*get_prompt(char **env);
+void		process_input(char *input, char ***env);
+int			main(int argc, char **argv, char **envp);
 
 // Pipes
-int count_commands_and_split(char *input, char ***commands);
-void setup_pipes_and_fork(int i, int cmd_count, int pipefd[2], int *prev_pipe);
-char **parse_args_and_handle(char *command, char **env);
-void child_exec_or_builtin(char *command, char ***env);
-int execute_pipeline(char *input, char **env);
+int			count_commands_and_split(char *input, char ***commands);
+void		setup_pipes_and_fork(int i, int cmd_count, int pipefd[2],
+				int *prev_pipe);
+char		**parse_args_and_handle(char *command, char **env);
+void		child_exec_or_builtin(char *command, char ***env);
+int			execute_pipeline(char *input, char **env);
 
 // Redirecciones
-int redirect_input(char *filename);
-int redirect_output(char *filename, int append);
-int heredoc(char *delimiter, char **env);
-void handle_redirections(char ***args, char **env);
+int			redirect_input(char *filename);
+int			redirect_output(char *filename, int append);
+int			heredoc(char *delimiter, char **env);
+void		handle_redirections(char ***args, char **env);
 
-//utils > expand_variable
-char *expand_variable(char *str, char **env);
-int process_env_variable(char *str, char *result, int *j, char **env);
+// utils > expand_variable
+char		*expand_variable(char *str, char **env);
+int			process_env_variable(char *str, char *result, int *j, char **env);
 
-//utils > find_user
-char	*find_user(char **env);
+// utils > find_user
+char		*find_user(char **env);
 
-//utils > autocomplete
-void free_command_names(void);
-void initialize_command_names(char **env);
-char *command_generator(const char *text, int state);
-char **command_completion(const char *text, int start, int end);
-void setup_autocomplete(char **env);
-
+// utils > autocomplete
+void		free_command_names(void);
+void		initialize_command_names(char **env);
+char		*command_generator(const char *text, int state);
+char		**command_completion(const char *text, int start, int end);
+void		setup_autocomplete(char **env);
 
 // Wildcards
-int contains_wildcard(const char *str);
-int match_pattern(const char *pattern, const char *filename);
-int count_matching_files(const char *pattern);
-char **expand_wildcards(const char *arg, int *num_expanded);
-char **expand_wildcards_in_args(char **args, int *num_args);
+int			contains_wildcard(const char *str);
+int			match_pattern(const char *pattern, const char *filename);
+int			count_matching_files(const char *pattern);
+char		**expand_wildcards(const char *arg, int *num_expanded);
+char		**expand_wildcards_in_args(char **args, int *num_args);
 
 #endif
