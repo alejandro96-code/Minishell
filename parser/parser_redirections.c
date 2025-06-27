c3f293c3a35be9a01b95dddf275a98898c55a57f
Alejandro <alejandro1996rd@gmail.com>
2025-06-27 17:30:01 +0200

    Ahora el comando cat archivo.txt > "qweqwe" | ls`

--- parser/parser_redirections.c
+++ parser/parser_redirections.c
@@ -3,40 +3,86 @@
 /*                                                        :::      ::::::::   */
 /*   parser_redirections.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dgasco-g <dgasco-g@student.42.fr>          +#+  +:+       +#+        */
+/*   By: alejanr2 <alejanr2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/26 22:43:18 by dgasco-g          #+#    #+#             */
-/*   Updated: 2025/06/26 23:51:57 by dgasco-g         ###   ########.fr       */
+/*   Updated: 2025/06/27 17:25:30 by alejanr2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-int	pipe_after_redirection_aux(const char *input, int i)
+int	skip_redirection_operator(const char *input, int i)
 {
-	int j;
+	int	j;
 
 	j = i;
-	printf("%c\n",input[i]);
+	if (input[j] && is_redirection(input[j]))
+	{
+		j++;
+		if (input[j] && is_redirection(input[j]) && input[j] == input[j - 1])
+			j++;
+	}
+	while (input[j] && ft_isspace(input[j]))
+		j++;
+	return (j);
+}
+
+int	skip_filename_with_quotes(const char *input, int i)
+{
+	int		j;
+	char	quote;
+	int		in_quotes;
+
+	j = i;
+	quote = '\0';
+	in_quotes = 0;
+	if (input[j] == '\"' || input[j] == '\'')
+	{
+		quote = input[j];
+		in_quotes = 1;
+		j++;
+	}
+	while (input[j])
+	{
+		if (in_quotes && input[j] == quote)
+		{
+			j++;
+			break ;
+		}
+		else if (!in_quotes && (ft_isspace(input[j]) || input[j] == '|'))
+			break ;
+		j++;
+	}
+	return (j);
+}
+
+int	pipe_after_redirection_aux(const char *input, int i)
+{
+	int	j;
+
+	j = skip_redirection_operator(input, i);
+	if (input[j])
+		j = skip_filename_with_quotes(input, j);
 	while (input[j] && ft_isspace(input[j]))
 		j++;
 	if (input[j] == '|')
 		return (1);
-	return (0);
+	return (1);
 }
 
 int	pipe_after_redirec(const char *input)
 {
-	int	i;
-	char quotes;
-	int in_quotes;
+	int		i;
+	char	quotes;
+	int		in_quotes;
 
 	i = 0;
 	quotes = '\0';
 	in_quotes = 0;
 	while (input[i])
 	{
-		if (!in_quotes &&(input[i] == '\"' || input[i] == '\''))
+		if (!in_quotes && (input[i] == '\"' || input[i] == '\''))
 		{
 			in_quotes = 1;
 			quotes = input[i];
@@ -45,16 +91,14 @@ int	pipe_after_redirec(const char *input)
 			in_quotes = 0;
 		if (is_redirection(input[i]) && !in_quotes)
 		{
-			if (!pipe_after_redirection_aux(input, i))
-			return (0);
+			pipe_after_redirection_aux(input, i);
 		}
-		
 		i++;
 	}
 	return (1);
 }
 
-void print_error_in_pipe(void)
+void	print_error_in_pipe(void)
 {
 	ft_putstr_fd("minishell: syntax error near unexpected token `|'\n", 2);
 }
